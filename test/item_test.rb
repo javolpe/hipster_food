419e78dfede3707a3bd0e6121e6d6b7f7040b0b8
Jacob Volpe <volpe.jacob@gmail.com>
2021-01-12 13:14:41 -0700

    Create item class, pass tests

--- test/item_test.rb
+++ test/item_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'pry'
+require './lib/item'
 
 class ItemTest < Minitest::Test
   def test_it_exists_and_has_attributes
@@ -8,6 +9,8 @@ class ItemTest < Minitest::Test
 
     assert_instance_of Item, item1
     assert_equal 3.75, item1.price
+    assert_equal Float, item1.price.class
     assert_equal "Peach Pie (Slice)", item1.name
+    assert_equal String, item1.name.class
   end
 end
