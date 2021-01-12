feae2e28a8c419e9a7e2d2a1715b62d4fd6536b2
Jacob Volpe <volpe.jacob@gmail.com>
2021-01-12 13:09:08 -0700

    Initial commit/make item tests

---
+++ test/item_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+
+class ItemTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+    assert_instance_of Item, item1
+    assert_equal 3.75, item1.price
+    assert_equal "Peach Pie (Slice)", item1.name
+  end
+end
