23f1dc382ddc0eb047f7aabf677f8ef8007d1d1f
Jacob Volpe <volpe.jacob@gmail.com>
2021-01-12 13:29:33 -0700

    Write foodtruck methods, add assertions, pass tests

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,10 +1,27 @@
 class FoodTruck
-  attr_reader :name,
-              :inventory
+  attr_reader :name
+
+  attr_accessor :inventory
 
   def initialize(name)
      @name = name
      @inventory = {}
+  end
+
+  def stock(item, stock)
+    if !inventory.keys.include?(item)
+      inventory[item] = stock
+    else 
+      inventory[item] += stock
+    end
+  end
+
+  def check_stock(item)
+    if inventory.keys.include?(item)
+      inventory[item]
+    else 
+      0
+    end
   end
 
 end
