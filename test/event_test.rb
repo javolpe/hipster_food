a62e0c68f4f9c2c2a4e34ffc4483df140adb32dd
Jacob Volpe <volpe.jacob@gmail.com>
2021-01-12 13:33:11 -0700

    Create event_tests

---
+++ test/event_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/item'
+require './lib/food_truck'
+require './lib/event'
+
+class EventTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    event = Event.new("South Pearl Street Farmers Market")
+
+    assert_instance_of Event, event
+    assert_equal "South Pearl Street Farmers Market", event.name 
+    assert_equal [], event.food_trucks
+  end
+end
